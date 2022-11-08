9c196e1e65900f4d6b43623a6e42899df2a45ac3
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-11-08 18:00:40 +0000

    used pipeline for error

--- _printf.c
+++ _printf.c
@@ -3,24 +3,27 @@ int _printf(const char *format, ...)
 {
 	va_list a;
 	int count = 0, i = 0;
-	va_start (a, format);
 
-	while(format[i])
+	va_start(a, format);
+	while (format[i])
 	{
-		if(format[i] == '%')
+		if (format[i] == '%')
 		{
 			i++;
-			switch(format[i])
+			switch (format[i])
 			{
 				case '%':
 					write(1, "%", 1);
 					count++;
 					break;
 				case 'c':
-					count+=print_char(va_arg(a, int));
+					count += print_char(va_arg(a, int));
 					break;
 				case 's':
-					count+=print_str(va_arg(a, char *));
+					count += print_str(va_arg(a, char *));
+					break;
+				case ' ': case '\0':
+					return (-1);
 					break;
 			}
 		}
@@ -32,5 +35,5 @@ int _printf(const char *format, ...)
 		i++;
 	}
 	va_end(a);
-	return(count);
+	return (count);
 }
