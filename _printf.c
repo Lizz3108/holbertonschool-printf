ba3600535be64efcd110a6f28bfe5fa2b46fe801
Lizbeth García Lebrón <lizbethgarcialebron@gmail.com>
2022-11-15 20:07:10 -0400

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _printf - functio that pritf formated output
+ * _printf - function that pritf formated output
  * @format: character string
- * Return: the number of character printed.
+ * Return: the number of character printed
  */
 int _printf(const char *format, ...)
 {
